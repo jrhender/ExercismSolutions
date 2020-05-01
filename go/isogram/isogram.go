648bfd7a7a3af985ca0f60792466461cbbae5c48
jrhender <johnhenderson45@gmail.com>
2020-05-01 07:54:28 -0700

    approved solution to isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,20 +1,22 @@
 // Package isogram provides functions relating to isograms
 package isogram
 
-import "strings"
+import (
+	"unicode"
+)
 
 // IsIsogram returns true if the input string is an isogram, false if it is not
 func IsIsogram(word string) bool {
-	set := make(map[rune]struct{}) // Using empty struct because consumes 0 bytes
-	exists := struct{}{}
-	for _, r := range strings.ToLower(word) {
-		if r == '-' || r == ' ' { // ok to have duplicate spaces or hyphens in isogram
+	set := make(map[rune]bool)
+	for _, r := range word {
+		if unicode.IsLetter(r) == false { // ok to have duplicate non-letter characters
 			continue
 		}
-		if _, ok := set[r]; ok { // set already contain letter so not an isogram
+		r = unicode.ToLower(r) // isograms are case insensitive
+		if set[r] {            // set already contain letter so not an isogram
 			return false
 		}
-		set[r] = exists
+		set[r] = true
 	}
 	return true // no duplicates found so it is an isogram
 }
