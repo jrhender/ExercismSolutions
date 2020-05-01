91117382c533eff5679a27682e0ac34cdd97cc63
jrhender <johnhenderson45@gmail.com>
2020-05-01 08:24:04 -0700

    shell of difference_of_squares

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,17 @@
+// Package diffsquares provides a function that calculates the difference of squares
+package diffsquares
+
+// SquareOfSum returns the square of the sum of the natural numbers from 1 to n
+func SquareOfSum(n int) int {
+	return 1
+}
+
+// SumOfSquares returns the sum of the squares of the natural numbers from 1 to n
+func SumOfSquares(n int) int {
+	return 1
+}
+
+// Difference returns the difference between the square of the sum and the sum of the squares of the natural numbers from 1 to n
+func Difference(n int) int {
+	return 1
+}
