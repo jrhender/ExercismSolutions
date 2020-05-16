d1e182332039d62280b0d26628be6aecd8cb2096
jrhender <johnhenderson45@gmail.com>
2020-05-16 11:15:39 -0700

    Further improvements to Luhn go

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -9,19 +9,22 @@ import (
 // Valid returns true if input string is a valid luhn number
 func Valid(input string) bool {
 	input = strings.ReplaceAll(input, " ", "")
-	sum := 0
-	runes := []rune(input)
-	l := len(runes)
-	if l <= 1 {
+
+	if len(input) <= 1 {
 		return false
 	}
-	evenIndex := false // Initialize as false because we're starting at 1 in the loop below
-	for i := 1; i <= l; i++ {
-		s := runes[l-i]
-		if unicode.IsDigit(s) == false {
+
+	evenIndex := false
+	if len(input)%2 == 0 {
+		evenIndex = true // Set to true because last number from right to left is even
+	}
+
+	sum := 0
+	for _, r := range input {
+		if unicode.IsDigit(r) == false {
 			return false
 		}
-		digit := int(s - '0') //ASCII digit codes are sequential from 0, so subtract 0 rune value to get digit
+		digit := int(r - '0') //ASCII digit codes are sequential from 0, so subtract 0 rune value to get digit
 		if evenIndex {
 			digit *= 2
 			if digit > 9 {
@@ -30,10 +33,6 @@ func Valid(input string) bool {
 		}
 		sum += digit
 		evenIndex = !evenIndex
-
 	}
-	if sum%10 == 0 {
-		return true
-	}
-	return false
+	return sum%10 == 0
 }
