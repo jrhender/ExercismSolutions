03f8825e8bb2f6342c3ad125cee5c48b0abd2a54
John Henderson <john.henderson@shareandcharge.com>
2020-09-06 09:34:49 -0700

    Simplified clock print and moved struct to top

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,6 +2,11 @@ package clock
 
 import "fmt"
 
+// Clock keeps track of time
+type Clock struct {
+	Minutes int
+}
+
 // New constructs new Clock
 func New(hour, minute int) Clock {
 	m := minute + hour*60
@@ -24,12 +29,5 @@ func (c Clock) Add(minutes int) Clock {
 
 // String returns clock value in 24 hour notation
 func (c Clock) String() string {
-	hours := (c.Minutes / 60) % 24
-	minutes := c.Minutes % 60
-	return fmt.Sprintf("%02d:%02d", hours, minutes)
-}
-
-// Clock keeps track of time
-type Clock struct {
-	Minutes int
+	return fmt.Sprintf("%02d:%02d", c.Minutes/60, c.Minutes%60)
 }
