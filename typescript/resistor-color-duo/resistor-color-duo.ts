8f19b526357c5733018bb2b9acdbcff454f709db
John Henderson <john.henderson@shareandcharge.com>
2020-10-10 14:49:54 -0700

    switched to using enum for typescript resistor-color

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,30 +1,30 @@
-const resistorBands: { [key: string]: number } = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
+enum ResistorBand {
+  black,
+  brown,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  violet,
+  grey,
+  white
 }
 
 export class ResistorColor {
-  private colors: string[];
+  private colors: ResistorBand[];
 
   constructor(colors: string[]) {
     if (colors.length < 2) {
       throw 'At least two colors need to be present'
     }
-    this.colors = colors;
+    const firstColor = colors[0] as keyof typeof ResistorBand;
+    const secondColor = colors[1] as keyof typeof ResistorBand;
+    this.colors = [ResistorBand[firstColor], ResistorBand[secondColor]];
   }
   
   value = (): number => {
     const [firstColor, secondColor] = this.colors;
-    const firstDigit: number = resistorBands[firstColor];
-    const secondDigit: number = resistorBands[secondColor];
-    return firstDigit * 10 + secondDigit
+    return firstColor.valueOf() *10 + secondColor.valueOf()
   } 
 }
