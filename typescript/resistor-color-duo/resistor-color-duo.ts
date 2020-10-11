f0c9a2c5b335e064881bbd6d93614c28a24f7b03
John Henderson <john.henderson@shareandcharge.com>
2020-10-11 11:20:35 -0700

    Using custom ResistorBandKey type

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -9,22 +9,24 @@ enum ResistorBand {
   violet,
   grey,
   white
-}
+};
+
+type ResistorBandKey = keyof typeof ResistorBand;
 
 export class ResistorColor {
   private colors: ResistorBand[];
 
-  constructor(colors: string[]) {
+  constructor(colors: ResistorBandKey[]) {
     if (colors.length < 2) {
       throw 'At least two colors need to be present'
     }
-    const firstColor = colors[0] as keyof typeof ResistorBand;
-    const secondColor = colors[1] as keyof typeof ResistorBand;
+    const firstColor = colors[0];
+    const secondColor = colors[1];
     this.colors = [ResistorBand[firstColor], ResistorBand[secondColor]];
   }
   
   value = (): number => {
     const [firstColor, secondColor] = this.colors;
-    return firstColor.valueOf() *10 + secondColor.valueOf()
+    return firstColor.valueOf() * 10 + secondColor.valueOf()
   } 
 }
