2de42446bc554861eb2c6f865c12922e3c0e43d1
John Henderson <john.henderson@shareandcharge.com>
2020-10-13 13:24:49 -0700

    initial passing version of typescript leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,18 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+
+function isLeapYear(year: number) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    else if (year % 100 == 0) {
+        return false;
+    }
+    else if (year % 4 == 0) {
+        return true;
+    }
+    return false;
 }
 
 export default isLeapYear
