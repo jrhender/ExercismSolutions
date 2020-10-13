f240bb369fedc93d24c379aaecde73e98de4d5b7
John Henderson <john.henderson@shareandcharge.com>
2020-10-13 13:33:13 -0700

    Improved commenting and added return type

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,8 +1,5 @@
-// on every year that is evenly divisible by 4
-//   except every year that is evenly divisible by 100
-//     unless the year is also evenly divisible by 400
-
-function isLeapYear(year: number) {
+// isLeapYear returns whether or not a year is a leap year.
+function isLeapYear(year: number): boolean {
     if (year % 400 == 0) {
         return true;
     }
