703d3c92c83912b9ed788f0227ff2fa09572fccb
John Henderson <john.henderson@shareandcharge.com>
2020-10-13 13:34:39 -0700

    slightly more succint version

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -6,10 +6,7 @@ function isLeapYear(year: number): boolean {
     else if (year % 100 == 0) {
         return false;
     }
-    else if (year % 4 == 0) {
-        return true;
-    }
-    return false;
+    return year % 4 == 0
 }
 
 export default isLeapYear
