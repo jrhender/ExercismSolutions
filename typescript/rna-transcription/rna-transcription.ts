203ff7fea0047ac7c2cb31c887482e411f7f1721
John Henderson <john.henderson@shareandcharge.com>
2020-10-14 13:56:11 -0700

    Try to do with enum

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,6 +1,15 @@
+enum DNAtoRNA {
+    G='C',
+    C='G',
+    T='A',
+    A='U'
+}
+type DNA = keyof typeof DNAtoRNA;
+
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    toRna(strand: string): string {
+        const DnaStrand = strand.split('') as  DNA[];
+        return DnaStrand.map(nucleotide => DNAtoRNA[nucleotide]).join();
     }
 }
 
