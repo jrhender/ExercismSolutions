c452f6337553132df09ba9d2da51915eb603dd28
John Henderson <john.henderson@shareandcharge.com>
2020-10-14 13:03:25 -0700

    moved function beneath core logic

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,8 +1,9 @@
 function isLeapYear(year: number): boolean {
-    const isDivisibleBy = (denominator: number): boolean => {
+    return (isDivisibleBy(4) && !isDivisibleBy(100)) || isDivisibleBy(400);
+
+    function isDivisibleBy(denominator: number): boolean {
         return year % denominator == 0;
     } 
-    return (isDivisibleBy(4) && !isDivisibleBy(100)) || isDivisibleBy(400);
 }
 
 export default isLeapYear
