f946587c45e21d246b872731953c40ec4bd32f1e
John Henderson <john.henderson@shareandcharge.com>
2020-10-14 13:00:43 -0700

    refactor to use flexible isDivisibleBy func and more succinct logic

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,23 +1,8 @@
 function isLeapYear(year: number): boolean {
-    if (isDivisibleBy4(year)) {
-        if (isDivisibleBy100(year)) {
-            return isDivisibleBy400(year)
-        }
-        return true;
-    }
-    return false;
+    const isDivisibleBy = (denominator: number): boolean => {
+        return year % denominator == 0;
+    } 
+    return (isDivisibleBy(4) && !isDivisibleBy(100)) || isDivisibleBy(400);
 }
-
-function isDivisibleBy4(year: number): boolean {
-    return year % 4 == 0;
-} 
-
-function isDivisibleBy100(year: number): boolean {
-    return year % 100 == 0;
-} 
-
-function isDivisibleBy400(year: number): boolean {
-    return year % 400 == 0;
-} 
 
 export default isLeapYear
