4049bdc08bc456534befd54c26781eeef36dd1b2
John Henderson <john.henderson@shareandcharge.com>
2020-10-14 08:46:57 -0700

    Made checks into functions

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,11 +1,23 @@
 function isLeapYear(year: number): boolean {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            return year % 400 == 0
+    if (isDivisibleBy4(year)) {
+        if (isDivisibleBy100(year)) {
+            return isDivisibleBy400(year)
         }
         return true;
     }
     return false;
 }
+
+function isDivisibleBy4(year: number): boolean {
+    return year % 4 == 0;
+} 
+
+function isDivisibleBy100(year: number): boolean {
+    return year % 100 == 0;
+} 
+
+function isDivisibleBy400(year: number): boolean {
+    return year % 400 == 0;
+} 
 
 export default isLeapYear
