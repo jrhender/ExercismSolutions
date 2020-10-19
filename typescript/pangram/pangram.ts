0d6af69ff12a825b31b8a95cd4d0a448d5de012b
John Henderson <john.henderson@shareandcharge.com>
2020-10-19 11:22:03 -0700

    working pangram

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,19 @@
+import { stringify } from "querystring";
+
+class Pangram {
+    constructor(private readonly input: string) {}
+
+
+    isPangram() {
+        const letters = new Map<string, boolean>();
+        for (let i = 97; i < 97 + 26; i++) {
+            letters.set(String.fromCharCode(i), false)
+        }
+        for (const c of this.input.toLowerCase()) {
+            letters.delete(c);
+        }
+        return letters.size == 0
+    }
+}
+
+export default Pangram
