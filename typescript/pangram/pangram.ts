c6ba8597c574a576a4a77940a9bef540e94dc749
John Henderson <john.henderson@shareandcharge.com>
2020-10-21 17:03:55 -0700

    another solution to pangram

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -2,20 +2,15 @@ class Pangram {
     constructor(private readonly input: string) {}
 
     isPangram() {
-        const letters = new Map<string, boolean>();
-        const firstCharCode = 'a'.charCodeAt(0);
-        const numLettersInAlphabet = 26
-        for (let i = firstCharCode; i < firstCharCode + numLettersInAlphabet; i++) {
-            letters.set(String.fromCharCode(i), false)
-        }
-
-        for (const c of this.input.toLowerCase()) {
-            if (letters.has(c)) {
-                letters.set(c, true)
-            }
-        }
-        return [...letters].filter(l => l[1] == false).length == 0
+        const loweredInput = this.input.toLowerCase()
+        return this.alphabet.every((c) => loweredInput.includes(c))
     }
+    
+    private readonly alphabet = new Array<string>(
+        'a','b','c','d','e','f','g','h','i','j','k',
+        'l','m','n','o','p','q','r','s','t','u','v',
+        'w','x','y','z'
+    )
 }
 
 export default Pangram
