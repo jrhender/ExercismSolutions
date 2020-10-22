16d86e54ef1d3c46b7f058b6d2f1196ca6488b20
John Henderson <john.henderson@shareandcharge.com>
2020-10-22 16:41:28 -0700

    feedback updates to matrix.ts

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,20 +1,18 @@
 class Matrix {
+    private transpose(matrix: TwoDMatrix): TwoDMatrix {
+        return matrix[0].map((_, i) => matrix.map(row => row[i]));
+    }
+
     constructor(matrixDescription: string) {
         const stringMatrix = matrixDescription.split('\n').map(r => r.split(" "))
-        this.rows = this.convertToNumberMatrix(stringMatrix)
+        this.rows = stringMatrix.map(r => r.map(c => Number(c)))
         this.columns = this.transpose(this.rows)
     }
 
-    readonly rows: number[][]
-    readonly columns: number[][]
-
-    private convertToNumberMatrix(matrix: string[][]): number[][] {
-        return matrix.map(r => r.map(c => parseInt(c)))
-    }
-
-    private transpose(matrix: number[][]): number[][] {
-        return matrix[0].map((_, i) => matrix.map(row => row[i]));
-    }
+    readonly rows: TwoDMatrix
+    readonly columns: TwoDMatrix
 }
+
+type TwoDMatrix = number[][]
 
 export default Matrix
