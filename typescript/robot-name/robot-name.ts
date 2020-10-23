e5fb326cdb951ea1ff36fb8410df8c7dd826215c
John Henderson <john.henderson@shareandcharge.com>
2020-10-23 16:30:20 -0700

    tests passing

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,11 +1,14 @@
+// Approach used is to view the "name" as a number between 100 and (26*26*999 - 100)
 class Robot {
-    private availableNumbers = Array.from({length: 26*26*1000}, (_, i) => i + 100)
+    private readonly alphabetSize = 26
+    private readonly mameMaximumNumber = 999
 
-    constructor() {
-        this.name = this.getNewName()
-    }
+    // availableNumbers are used to keep track of numbers which have been used
+    private availableNumbers = Array.from(
+        {length: this.alphabetSize*this.alphabetSize*this.mameMaximumNumber},
+        (_, i) => i)
 
-    name: string
+    name = this.getNewName()
 
     resetName() {
         this.name = this.getNewName()
@@ -17,13 +20,14 @@ class Robot {
         return this.getName(newNumber)
     }
 
+    // getName approach is inspired by hex to decimal conversion
     private getName(source: number) {
         const convertToLetter = (n: number) => String.fromCharCode(n + 65)
         const firstLetter = convertToLetter(source%26)
         const firstQuotient = Math.floor(source/26) 
         const secondLetter = convertToLetter(firstQuotient%26)
         const secondQuotient = Math.floor(firstQuotient/26)
-        const number = secondQuotient%1000
+        const number = ('00' + secondQuotient%this.mameMaximumNumber).slice(-3)
         return firstLetter + secondLetter + number 
     }
 
