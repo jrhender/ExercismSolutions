33d2b340845c2edf789cd7bea84edaa98c1071bb
John Henderson <john.henderson@energyweb.org>
2020-12-26 14:21:13 -0800

    passing c armstrong number

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,27 @@
 #include "armstrong_numbers.h"
+#include <math.h>
+
+bool is_armstrong_number(int candidate)
+{
+
+    // Get count
+    int value = candidate;
+    double count = 0;
+    while (value > 0)
+    {
+        value /= 10;
+        count++;
+    }
+
+    // Sum exponentiated digits
+    double sum = 0;
+    int value2 = candidate;
+    while (value2 > 0)
+    {
+        int digit = value2 % 10;
+        sum += pow( (double) digit, count);
+        value2 /= 10;
+    }
+    
+    return (double) candidate == sum;
+}
