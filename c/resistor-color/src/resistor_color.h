266014149ff70e32b969912e99244d4b2d2c7823
John Henderson <john.henderson@energyweb.org>
2021-01-03 19:22:02 -0800

    passing resistor_color c

--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,7 +2,20 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
 } resistor_band_t;
+
+int color_code(resistor_band_t color);
+
+resistor_band_t* colors();
 
 #endif
